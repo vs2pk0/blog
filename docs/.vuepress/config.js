7b4a82139d9a0def35d6720c061b99b798225047
vs2pk0 <904239787@qq.com>
2019-09-11 09:59:59 +0800

    搭建vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: '小米不是米的文档',
+  description: '小米不是米的文档, vuepress 文档',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }]
+  ]
+}
