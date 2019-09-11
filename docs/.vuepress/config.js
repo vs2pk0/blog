8487584c891f6f732cae4e91d21149f8add5418e
vs2pk0 <904239787@qq.com>
2019-09-11 13:39:01 +0800

    设置配置文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,7 @@
+const { mdConf, themeConf, localesConf } = require('./config/')
 module.exports = {
-  //   base: 'https://www.heigouxiong.cn/vuepress',
-  base: '/',
-  cache: false,
-  title: '小米不是米的文档',
-  description: '小米不是米的文档, vuepress 文档',
-  head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }]
-  ]
+  locales: localesConf,
+  markdown: mdConf,
+  themeConfig: themeConf,
+  plugins: ['@vuepress/back-to-top']
 }
---
+++ docs/pages/guide.md
@@ -0,0 +1,8 @@
+---
+title: '导航'
+permalink: 'guide'
+comment: true
+single: true
+---
+
+### 你好啊
